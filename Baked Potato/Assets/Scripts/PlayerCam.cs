518c5cd36ad99c4f31d5c648a46f4e5c03acf3c2
Johan <jdsilla@cpp.edu>
2018-03-04 16:05:10 -0800

    Add Follow Cam, Player, and floor to test level

---
+++ Baked Potato/Assets/Scripts/PlayerCam.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour {
+
+    public Transform player;
+    public Vector3 offset = new Vector3 (2, 3, 2);
+
+	void Update () {
+        transform.position = player.position + offset;
+	}
+}
---
+++ Baked Potato/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    Rigidbody rb;
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("s")) {
+            rb.velocity = new Vector3(10, 0, 10);
+        }
+	}
+}
