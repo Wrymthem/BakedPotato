2ea3d4fbafd6850ce3e456fb41d525ce2a69d6be
Johan <jdsilla@cpp.edu>
2018-03-04 19:58:43 -0800

    Trying grid based player movement

--- Baked Potato/Assets/Scripts/PlayerMovement.cs
+++ Baked Potato/Assets/Scripts/PlayerMovement.cs
@@ -4,29 +4,43 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-
     public Rigidbody rb;
+    private float lastMove;
+    public float moveDelay = 0.1f;
+    public float interpolationSpeed = 10.0F;
+    Vector3 nextPosition;
+
+    // Use this for initialization
+    void Start () {
+        nextPosition = transform.position;
+        rb.freezeRotation = true;
+	}
 
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey("s")) {
-            rb.velocity = new Vector3(10, 0, 10);
-        }
-        if (Input.GetKey("w")) {
-            rb.velocity = new Vector3(-10, 0, -10);
-        }
-        if (Input.GetKey("a")) {
-            rb.velocity = new Vector3(10, 0, -10);
-        }
-        if (Input.GetKey("d")) {
-            rb.velocity = new Vector3(-10, 0, 10);
-        }
-        if (Input.GetKeyUp("s") || Input.GetKeyUp("w") || Input.GetKeyUp("a") || Input.GetKeyUp("d")) {
-            rb.velocity = new Vector3(0, 0, 0);
+        wasdMovement();
+        transform.position = Vector3.Lerp(transform.position, nextPosition, interpolationSpeed * Time.deltaTime);
+    }
+
+    void wasdMovement() {
+        if (Time.time - lastMove > moveDelay) {
+            if (Input.GetKey("w")) {
+                nextPosition += new Vector3(-1, 0, -1);
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            if (Input.GetKey("a")) {
+                nextPosition += new Vector3(1, 0, -1);
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            if (Input.GetKey("s")) {
+                nextPosition += new Vector3(1, 0, 1);
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            if (Input.GetKey("d")) {
+                nextPosition += new Vector3(-1, 0, 1);
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            lastMove = Time.time;
         }
     }
 }
