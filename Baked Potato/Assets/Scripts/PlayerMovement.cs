e24f4061aae870d33606f93b42b7da52626ed821
Johan <jdsilla@cpp.edu>
2018-03-04 16:16:28 -0800

    Finish player movement

--- Baked Potato/Assets/Scripts/PlayerMovement.cs
+++ Baked Potato/Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,24 @@ public class PlayerMovement : MonoBehaviour {
 		
 	}
 
-    Rigidbody rb;
+    public Rigidbody rb;
 
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKey("s")) {
             rb.velocity = new Vector3(10, 0, 10);
         }
-	}
+        if (Input.GetKey("w")) {
+            rb.velocity = new Vector3(-10, 0, -10);
+        }
+        if (Input.GetKey("a")) {
+            rb.velocity = new Vector3(10, 0, -10);
+        }
+        if (Input.GetKey("d")) {
+            rb.velocity = new Vector3(-10, 0, 10);
+        }
+        if (Input.GetKeyUp("s") || Input.GetKeyUp("w") || Input.GetKeyUp("a") || Input.GetKeyUp("d")) {
+            rb.velocity = new Vector3(0, 0, 0);
+        }
+    }
 }
